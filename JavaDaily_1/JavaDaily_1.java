c6ef82895fbcbb8d2d7e66326c49ccd3a1fbefef
Maria61 <mariaLi61@outlook.com>
2019-05-11 21:30:13 +0800

    数据类型练习2

---
+++ JavaDaily_1/JavaDaily_1.java
@@ -0,0 +1,28 @@
+public class JavaDaily_1{
+	public static void main(String[] args){
+		/*System.out.println("Hello,Java");
+		
+		int num=10;
+		System.out.println(num*2);
+		
+		int maxValue=Integer.MAX_VALUE;
+		int minValue=Integer.MIN_VALUE;
+		System.out.println("maxValue="+maxValue);
+		System.out.println("minValue="+minValue);
+		System.out.println("maxValue+1="+(maxValue+1));
+		System.out.println("minValue-1="+(minValue-1));
+		
+		long num=(maxValue+1L);//1后必须加L或l
+		System.out.println(num);*/
+		
+		int a=100;
+		byte b=(byte)a;//不是直接存进byte里的需要强转！不然会报错！
+		//byte c=1299;
+		//System.out.println(c);
+		
+		int d=300;
+		byte e=(byte)d;
+		System.out.println(e);//44  ?
+		
+	}
+}
