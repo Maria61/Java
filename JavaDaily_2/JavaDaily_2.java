57e34521d22185f17fa5942ca21faf2c6d11792a
Maria61 <mariaLi61@outlook.com>
2019-05-12 18:38:00 +0800

    数据类型练习2

---
+++ JavaDaily_2/JavaDaily_2.java
@@ -0,0 +1,44 @@
+public class JavaDaily_2{
+	
+	public static void main(String[] args){
+		/*int a=511;
+		byte b=(byte)a;
+		System.out.println(a);
+		System.out.println(b);
+		
+		long c=2147483651L;//大于int型最大值！
+		int d=(int)c;
+		System.out.println(c);
+		System.out.println(d);*/
+		
+		//System.out.println(11+1L);//12
+		
+		//double num=10.2;
+		//System.out.println(num*2);//20.4
+		
+		//System.out.println(1.1*1.1);//1.2100000000000002
+		
+		/*int numA=10;
+		int numB=4;
+		System.out.println(numA/numB);
+		System.out.println(numA/(double)numB);
+		System.out.println((double)numA/numB);*/
+		
+		/*char c='A';
+		int num=c;
+		System.out.println(num);//65
+		
+		char a=(char)num;
+		System.out.println(a);//A
+		*/
+		
+		/*char c='A';
+		int num=c+32;
+		char x=(char)num;
+		System.out.println(x);//a
+		*/
+		
+		System.out.println("哈哈\n我好饿\t啊！\n\"吃饭啦吃饭啦，今天就先练习到这！");
+		
+	}
+}
