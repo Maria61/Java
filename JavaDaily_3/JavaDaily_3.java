ae92660b66b2387afb1483ca2a94fec47611407f
Maria61 <mariaLi61@outlook.com>
2019-05-13 19:53:54 +0800

    运算符基本练习1

---
+++ JavaDaily_3/JavaDaily_3.java
@@ -0,0 +1,38 @@
+public class JavaDaily_3{
+	public static void main(String[] args){
+		//++    --
+		/*int x=3;
+		int y=4;
+		System.out.println((x++)*y);//12
+		System.out.println(x);//4
+		System.out.println((++x)*y);//20
+		System.out.println(x);//5
+		*/
+		
+		//三目运算符
+		/*int x=3;
+		int y=4;
+		int z=3>4?x+y:x-y;//-1
+		System.out.println(z);*/
+		
+		//关系运算符
+		/*char a='哈';
+		int num=100;
+		int num=10000000;//false
+		System.out.println(a>num);//true
+		*/
+		
+		//逻辑运算符
+		/*if(1<2|(10/0==0)){
+			System.out.println("满足！");
+		}
+		if(1<2||(10/0==0)){
+			System.out.println("manzu");
+		}*/
+		
+		//位运算符
+		System.out.println(2&8);//0
+		System.out.println(2|8);//10
+		
+	}
+}
