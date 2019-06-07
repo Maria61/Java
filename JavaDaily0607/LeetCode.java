7294450cedf1807c5a764dac8e4f3d47d98fae4d
Maria61 <mariaLi61@outlook.com>
2019-06-08 00:13:19 +0800

    添加注释

--- JavaDaily0607/LeetCode.java
+++ JavaDaily0607/LeetCode.java
@@ -4,7 +4,14 @@ class ListNode {
     ListNode(int x) { val = x; }
 }
 public class LeetCode{
-
+	/**问题描述：请判断一个链表是否为回文链表。
+	**/
+	/*思路：
+		将链表值取出组成十进制数字，分别是
+		按链表的顺序,先取出的数为高位，后取出的数为低位，组成一个数a;
+		按链表顺序，先取出的数为低位，后取出的数为高位，组成一个数b;
+		比较两数是否相等，相等，返回true，为回文链表，不相等返回false；
+	*/
 	public static boolean isPalindrome(ListNode head) {
         int a=0;
         int b=0;
