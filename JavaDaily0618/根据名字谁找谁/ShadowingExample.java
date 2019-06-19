986f9b6ca60246a7e37dbd0e7b0abe69b8cfd7e4
Maria61 <mariaLi61@outlook.com>
2019-06-19 21:12:16 +0800

    添加注释

--- JavaDaily0618/根据名字谁找谁/ShadowingExample.java
+++ JavaDaily0618/根据名字谁找谁/ShadowingExample.java
@@ -2,8 +2,8 @@ class A{
 	int size=5;
 	
 	public void method(int size){
-		System.out.println(size);
-		System.out.println(this.size);
+		System.out.println(size);	//形参	打印100
+		System.out.println(this.size);	//属性  打印5
 	}
 }
 
