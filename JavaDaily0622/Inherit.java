ff6e384720cf10708a16aae04bfea50863395f22
Maria61 <mariaLi61@outlook.com>
2019-06-22 21:34:12 +0800

    “继承”练习

---
+++ JavaDaily0622/Inherit.java
@@ -0,0 +1,35 @@
+class A{
+	int a=10;
+	static int b=10;
+	
+	public void method(int a){
+		a=20;	//Shadowing
+		System.out.println(a);
+		System.out.println(this.a);
+	}
+	public static void staticMethod(){
+		System.out.println("父类的静态方法");
+	}
+}
+
+class B extends A{
+	int a=30;
+	static int b=30;
+	
+	public static void staticMethod(){
+		System.out.println("子类的静态方法");
+	}
+}
+
+public class Inherit{
+	public static void main(String[] args){
+		B b=new B();
+		System.out.println(b.a);//30
+		System.out.println(b.b);//30
+		b.method(5);//20,10
+		B.staticMethod();//子类的静态方法
+		A.staticMethod();//父类的静态方法
+		
+		
+	}
+}
